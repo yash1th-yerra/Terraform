7be4422ac6f3b9bebdfde3495682853a277d50dd
yash1th-yerra <yashwanthyash7ms@gmail.com>
2024-12-26 22:26:14 +0530

    Terraform IaC

---
+++ module/ec2/ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "ap-south-1"
+
+}
+
+resource "aws_instance" "example" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
+}
---
+++ module/ec2/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value= "ami-053b12d3152c0cc71"
+instance_type_value= "t2.micro"
+subnet_id_value="subnet-0d9e6620bea482d31"
